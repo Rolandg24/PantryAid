da8586ca78f187ccdc2e84401f05d4292bb39b54
Rolandg24 <garza.roland24@gmail.com>
2025-02-17 22:14:57 -0500

    Fixed bug error in the js.config

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api', // Adjust if needed
+  baseURL: '/api', // This ensures Vite forwards the request to the backend
   headers: {
     'Content-Type': 'application/json'
   }
